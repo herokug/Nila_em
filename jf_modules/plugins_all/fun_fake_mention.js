ad4c764a343a97c8d5da83f314236425851679b1
Ravindu Dilshan <90592440+ravindudil5han@users.noreply.github.com>
2022-10-06 21:17:58 +0530

    Update fun_fake_mention.js

--- jf_modules/plugins_all/fun_fake_mention.js
+++ jf_modules/plugins_all/fun_fake_mention.js
@@ -22,12 +22,25 @@ async (m, conn , { jf }) => {
 
 
     try {
+       let victim = m.text.split("/")[0].replace(/ /g,'').replace(/-/g, '').replace('(' , '').replace(')' , '')
+
+    if (victim.length == 10 ) {
+        victim = '94'+victim.slice(1);
+    }
+    
+    if (victim.startsWith('+')) { 
+        victim = victim.slice(1);
+    }
+
+       
+       let victim_num = victim
+    let victim_id = victim.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
 
        
        // simple eken jf.victim_id 
 
     const group_jid = m.text.split('/')[1].split('[')[0]
-    const tagged = jf.victim_id
+    const tagged = victim_id
     const tag_m = m.text.split('[')[1].split(']')[0]
     
     let m2
@@ -56,7 +69,7 @@ const msgs = {
       remoteJid: group_jid,
       fromMe: false,
       id: '',
-      participant: jf.victim_id
+      participant: victim_id
     },
     message: Message = { conversation: tag_m },
 }
